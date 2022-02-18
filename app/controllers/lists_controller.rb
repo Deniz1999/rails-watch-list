fb8bbc253d20613579265297b137b9df933c0656
Deniz Kazdal <denizkazdal99@gmail.com>
2022-02-18 11:57:53 +0000

    Completed Backend

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,15 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    # @review = Review.new
+    # @review.list = @list
   end
 
   def new
@@ -10,14 +19,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
-  def show
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
     @list = List.find(params[:id])
-    @bookmark = Bookmark.new
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
   end
 end
